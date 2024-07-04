6cd2044577051fdf8807a172164ee1292a136da8
Raghavb175 <raghavbhatia1705@outlook.com>
2024-07-04 14:12:41 -0600

    nav

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar></Navbar>
-      <div className="container">
-        <div className="bg-red-600">Hey I am red</div>
-      </div>
+    <Navbar/>
+      <nav className="container">
+        <div className="bg-red-600">Your To-Dos</div>
+      </nav>
     </>
   )
 }
--- my-project/src/components/navbar.jsx
+++ my-project/src/components/navbar.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-export const navbar = () => {
+ const Navbar = () => {
   return (
-    <div>Hey I am a navbar</div>
+    <nav className='flex justify-between text-white bg-slate-400 py-3 '>
+      <div className="logo">
+        <span className='font-bold texl-xl mx-10'>I logo</span>
+      </div>
+      <ul className='flex gap-8 mx-10'>
+        <li className='cursor-pointer'>Home</li>
+        <li className='cursor-pointer'>To Do</li>
+      </ul>
+    </nav>
   )
 }
+export default Navbar

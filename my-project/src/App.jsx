dc605df2e082e2d4e0c0f3f3f797e91dc45758c2
Raghavb175 <raghavbhatia1705@outlook.com>
2024-07-04 13:35:26 -0600

    starting

---
+++ my-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar></Navbar>
+      <div className="container">
+        <div className="bg-red-600">Hey I am red</div>
+      </div>
+    </>
+  )
+}
+
+export default App
